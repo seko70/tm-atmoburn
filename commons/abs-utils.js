0574654fe24bead3f555f27294a49817df0696c2
Senderák, Rastislav <rastislav.senderak@bootiq.io>
2026-02-09 14:07:58 +0100

    feature: commns/abs-utils: added "absDirections" as a generalized "absElevation"; absElevation funkction is now DEPRECATED

--- commons/abs-utils.js
+++ commons/abs-utils.js
@@ -1,8 +1,74 @@
-/* abs-utils - common utility functions without dependencies, shared by several scripts; version 1.1.0 */
+/* abs-utils - common utility functions without dependencies, shared by several scripts; version 1.2.0 */
 (function (global) {
     'use strict';
 
-    // --- from two locations (with x,y,z attributes) compute direction (o'clock) and elevation (degrees)
+    global.absDirections = function (p1, p2) {
+        const ARROWS = ["↑", "↗", "→", "↘", "↓", "↙", "←", "↖"];
+
+        // convert radians to degrees
+        const rad2deg = function (angle) {
+            return angle * 57.29577951308232; // angle / Math.PI * 180
+        }
+
+        // convert horizontal direction in degrees to arrow
+        const arrowFromDeg = function (deg) {
+            if (deg == null) return '-';
+            const normailizedDeg = ((deg % 360) + 360) % 360;
+            return ARROWS[Math.floor((normailizedDeg + 22.5) / 45) % 8];
+        }
+
+        // convert horizontal direction in degrees to compas direction (S,N,E,W,...)
+        const compassFromDeg = function (deg) {
+            return (deg == null) ? '-' : ["N", "NE", "E", "SE", "S", "SW", "W", "NW"][Math.round(deg / 45) % 8];
+        }
+
+        // convert horizontal direction in degrees to clock notation
+        const clockFromDeg = function (deg) {
+            if (deg == null) return '-';
+            const horiz = Math.round((deg - 90 + 180) / 360 * 12);
+            return (horiz < 0) ? horiz + 12 : (horiz < 1) ? 12 : horiz;
+        }
+
+        // return horizontal direction in degrees (0-180), 0 is "north"
+        const horizontalDeg = function (p1, p2) {
+            if (p1 == null || p2 == null) return null; // no data
+            const dx = p2.x - p1.x;
+            const dy = p2.y - p1.y;
+            if (dx === 0 && dy === 0) return null; // no direction
+            const deg = rad2deg(Math.atan2(dx, dy));
+            return Math.round(deg < 0 ? deg + 360 : deg);
+        }
+
+        // return vertical direction, in degrees (-90,90)
+        const verticalDeg = function (p1, p2) {
+            if (p1 == null || p2 == null) return null; // no data
+            const dz = p2.z - p1.z;
+            if (dz === 0) return 0;
+            const dx = p2.x - p1.x;
+            const dy = p2.y - p1.y;
+            const dxy = Math.round(Math.sqrt(dx * dx + dy * dy));
+            if (dxy === 0) return dz > 0 ? 90 : -90;
+            let vert = Math.round(rad2deg(Math.atan2(dz, dxy)));
+            if (vert < -90) {
+                vert = -180 - vert;
+            } else if (vert > 90) {
+                vert = 180 - vert;
+            }
+            return vert;
+        }
+
+        const h = horizontalDeg(p1, p2);
+        const v = verticalDeg(p1, p2);
+        return {
+            h: h,
+            v: v,
+            clock: clockFromDeg(h),
+            compass: compassFromDeg(h),
+            arrow: arrowFromDeg(h)
+        }
+    }
+
+    // --- (DEPRECATED) from two locations (with x,y,z attributes) compute direction (o'clock) and elevation (degrees)
     global.absElevations = function (p1, p2) {
 
         function _rad2deg(angle) {
@@ -83,6 +149,16 @@
         return s ? s.trim().split(/\s+/).pop() : null;
     }
 
+    // Returns last element of array/list.
+    global.last = function (array) {
+        return array && array.length > 0 ? array[array.length - 1] : null;
+    }
+
+    // Returns default value if input is empty/null/undefined.
+    global.useDefault = function (s, defaultValue = '???') {
+        return s && s.length > 0 ? s : defaultValue;
+    }
+
     // Returns true if inputeDate (of type Date, or null) is older than nowDate (of type Date) by more than maxDifSeconds (in seconds).
     global.isOlderThan = function (inputDate, nowDate, maxDifSeconds) {
         if (!inputDate) return true; // date not set or unknown we want to by true/older
@@ -94,6 +170,5 @@
     global.assert = function (condition, message = 'Assertion failed') {
         if (!condition) throw new Error(message);
     }
-
 
 })(typeof window !== 'undefined' ? window : this);
