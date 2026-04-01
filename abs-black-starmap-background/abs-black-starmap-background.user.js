ebc7a550a1e4fa10c6dcda0b9aad2ed58983e679
Senderák, Rastislav <rastislav.senderak@bootiq.io>
2026-04-01 21:52:07 +0200

    fix: fixed update URL(s)

--- abs-black-starmap-background/abs-black-starmap-background.user.js
+++ abs-black-starmap-background/abs-black-starmap-background.user.js
@@ -2,12 +2,12 @@
 // @name         Black Starmap Background
 // @namespace    sk.seko
 // @description  Changes background image to a black one in WarFacts/Atmoburn Starmap; original by guardian
-// @updateURL    https://github.com/seko70/tm-atmoburn/raw/refs/heads/main/abs-black-starmap-background/abs-tag-manager.user.js
-// @downloadURL  https://github.com/seko70/tm-atmoburn/raw/refs/heads/main/abs-black-starmap-background/abs-tag-manager.user.js
+// @updateURL    https://github.com/seko70/tm-atmoburn/raw/refs/heads/main/abs-black-starmap-background/abs-black-starmap-background.user.js
+// @downloadURL  https://github.com/seko70/tm-atmoburn/raw/refs/heads/main/abs-black-starmap-background/abs-black-starmap-background.user.js
 // @homepageURL  https://github.com/seko70/tm-atmoburn/blob/main/abs-black-starmap-background/README.md
 // @license      MIT
 // @match        https://*.atmoburn.com/extras/view_universe.php*
-// @version      2.0.3
+// @version      2.0.4
 // @grant        none
 // ==/UserScript==
 
